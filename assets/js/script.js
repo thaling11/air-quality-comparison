e38e84e50774c2d899cfdadbaaea988223501a70
Levi H <levi.heintzelman@gmail.com>
2022-08-01 15:25:21 -0600

    Restructure JS and add Air Q to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,6 @@
 // API key
 var airQualityAPIkey = "4929c896-1465-4dd5-927c-6506a0034f03";
 var airResults = document.getElementById("air-results");
-
-let cityName;
-
-//current air quality element
-let airQualityConditions = document.querySelector("#air-q");
-
-//use Geolocation API to get user location on entering site
-function getGeolocation() {
-    if (navigator.geolocation) {
-        console.log('geolocation available');
-        navigator.geolocation.getCurrentPosition(function(position){
-            console.log(position);
-        });
-    }
-}
-getGeolocation();
-    
-//get nearest city data 
-// let lat = coords.latitude;
-// let long = coords.longitude;
-
-function getAirQuality() {
-    let requestUrl = `http://api.airvisual.com/v2/city?city=${cityName}&state=${stateInput}&country=USA&key=${airQualityAPIkey}`;
-    
-    fetch(requestUrl)
-    .then(function(response) {
-        return response.json()
-    }).then(function(data) {
-        
-        console.log(data);
-        
-        // airResults.textContent= `Air Quality: ${data.current.pollution.aqius}`;
-        // console.log(data.current.pollution.aqius);
-        
-    })
-    return;
-}
-
-//get nearest city data (IP geolocation)
-// http://api.airvisual.com/v2/nearest_city?key={{YOUR_API_KEY}}
-
-//get nearest city data
-//http://api.airvisual.com/v2/nearest_city?lat={{LATITUDE}}&lon={{LONGITUDE}}&key={{YOUR_API_KEY}}
-
-// Event Listener to search Button
-// Event Listener to Hospitals
-    // Event listener to current location
-// Set up Air quality API fetch based on search
-// Set up hosipital API  fetch
-// Use Local Storage *****
-function storeLocation() {
-    let cityName = document.querySelector("#search-input").value;
-    e = document.querySelector("#state-select");
-    stateValue = e.options[e.selectedIndex].value;
-    stateInput = e.options[e.selectedIndex].text;
-    localStorage.setItem(stateInput,JSON.stringify(cityName));
-}
-// Save search in local storage to then apply it to the find hospitals??
-    // pull 3 closest hospitals initially
-    // find closest hosptital
-    //https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API#examples
-
-            //Need distance calculator from lats and longs
-            //This function uses distance as the crow flies
-            
-
-// Add information about poor air quality / images of smog
-
 //buttons
 let search = document.querySelector("#check");
 let hospitalFinder = document.querySelector("#find");
@@ -88,21 +20,92 @@ let lon1;
 let lat2;
 let lon2;
 
+let cityName;
+
+//current air quality element
+let airQualityConditions = document.querySelector("#air-q");
+
+//use Geolocation API to get user location on entering site
+function getGeolocation() {
+    if (navigator.geolocation) {
+        console.log('geolocation available');
+        navigator.geolocation.getCurrentPosition(function(position){
+            console.log(position);
+        });
+    }
+}
+getGeolocation();
+    
+
+
+function getAirQuality() {
+    let requestUrl = `http://api.airvisual.com/v2/city?city=${cityName}&state=${stateInput}&country=USA&key=${airQualityAPIkey}`;
+    
+    fetch(requestUrl)
+    .then(function(response) {
+        return response.json()
+    }).then(function(data) {
+        storeAir(data);
+        
+        return (data);       
+    })
+    return;
+}
+
+// store air quality in local storage
+function storeAir(data) {
+    let aqius = data.data.current.pollution.aqius;
+    let mainus = data.data.current.pollution.mainus;
+
+    let currentCityScore = {
+        aqi: aqius,
+        main: mainus
+    };
+    localStorage.setItem(cityName, JSON.stringify(currentCityScore));
+};
+
+
+//get nearest city data 
 function getLocalAir(){
     
-    console.log ("This button works too!")
     let localURL = `http://api.airvisual.com/v2/nearest_city?key=${airQualityAPIkey}`;
 
     fetch (localURL)
     .then(function(response) {
         return response.json()
-    }).then(function(data) {
-        
-        console.log(data);
+    }).then(function(data2) {
+        console.log(data2);
+        storeLocalAir(data2);
+        return (data2);
     })
 }
 
+//Store local air quality in local storage
+function storeLocalAir(data2) {
+    let aqius = data2.data.current.pollution.aqius;
+    let mainus = data2.data.current.pollution.mainus;
 
+    let currentCityScore = {
+        aqi: aqius,
+        main: mainus
+    };
+    localStorage.setItem(data2.data.city, JSON.stringify(currentCityScore));
+};
+
+// function getLife(){
+//     let lifeURL = 'https://api.teleport.org/apicities/?search=Palo%20Alto%2C%20California&embed=city%3Asearch-results%2Fcity%3Aitem%2Fcity%3Aurbanarea%2Fua%3Ascores'
+//     fetch (lifeURL)
+//     .then(function(response) {
+//         return response.json()
+//     }).then(function(data) {
+        
+//         console.log(data);
+//     })
+
+// }
+
+
+// event listener on search button
 search.addEventListener("click", function(event) {
     cityName = document.querySelector("#search-input").value
     e = document.querySelector("#state-select");
@@ -110,5 +113,8 @@ search.addEventListener("click", function(event) {
     stateInput = e.options[e.selectedIndex].text;
     getAirQuality();
     getLocalAir();
-    storeLocation();
+    // getLife();
+    
 });
+
+
